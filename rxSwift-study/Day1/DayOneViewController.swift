b119ff430fbb78ea688fe1cd356d3052f05e0b2e
dagad <dagad@estsoft.com>
2018-12-06 17:16:20 +0900

    try determine double tap on button

--- rxSwift-study/Day1/DayOneViewController.swift
+++ rxSwift-study/Day1/DayOneViewController.swift
@@ -15,6 +15,12 @@ class DayOneViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var label: UILabel!
     
+    @IBOutlet weak var filterLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textFieldLabel: UILabel!
+    
     var disposeBag = DisposeBag()
     var labelCount = 0
     
@@ -25,13 +31,25 @@ class DayOneViewController: UIViewController {
             .tap
             .bind(onNext: tapButton)
             .disposed(by: disposeBag)
+        
+        textField.rx
+            .text
+            .subscribe(typeText)
+            .disposed(by: disposeBag)
+        
+        
     }
-
 }
 
 extension DayOneViewController {
     private func tapButton() {
         labelCount += 1
         label.text = String(labelCount)
+    }
+    
+    private func typeText(_ event: Event<String?>) {
+        if let text = event.element as? String {
+            textFieldLabel.text = text
+        }
     }
 }
